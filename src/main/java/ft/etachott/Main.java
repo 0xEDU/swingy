658ef39946ddb6d8b536be6027682ec63fd5ed6f
etachott <bfwaend@gmail.com>
2024-03-03 21:19:56 -0300

    Add simple argument validation

--- src/main/java/ft/etachott/Main.java
+++ src/main/java/ft/etachott/Main.java
@@ -1,7 +1,17 @@
 package ft.etachott;
 
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 1) {
+            System.err.println("Usage: java -jar swingy.jar [console | gui]");
+        }
+        if (Objects.equals(args[0], "console")) {
+            System.out.println("console");
+        } else if (Objects.equals(args[0], "gui")) {
+            System.out.println("gui!");
+        }
+
     }
 }
