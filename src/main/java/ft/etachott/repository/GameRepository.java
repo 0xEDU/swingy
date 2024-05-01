8306ae5e2565f30dbc485c7f1dfcbe9326b9535d
etachott <bfwaend@gmail.com>
2024-05-01 20:12:07 -0300

    Add 'save' function to GameRepository

--- src/main/java/ft/etachott/repository/GameRepository.java
+++ src/main/java/ft/etachott/repository/GameRepository.java
@@ -1,5 +1,9 @@
 package ft.etachott.repository;
 
+import ft.etachott.model.Character;
+
 public class GameRepository {
     public GameRepository() {}
+
+    public void save(Character character) {}
 }
