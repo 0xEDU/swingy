085d58886f4c941da4967f97518b1b8ab9d68cc2
etachott <bfwaend@gmail.com>
2024-04-30 22:10:12 -0300

    Add GameService class

---
+++ src/main/java/ft/etachott/service/GameService.java
@@ -0,0 +1,12 @@
+package ft.etachott.service;
+
+import ft.etachott.repository.GameRepository;
+
+public class GameService {
+    final private GameRepository _gameRepository = new GameRepository();
+    public GameService() {}
+
+    void createHero() {
+
+    }
+}
