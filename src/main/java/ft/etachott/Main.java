24b4e73cc05b044d1a920d6778d6418ee874e6ab
etachott <bfwaend@gmail.com>
2024-06-07 21:40:18 -0300

    Update main to SpringBootApplication

--- src/main/java/ft/etachott/Main.java
+++ src/main/java/ft/etachott/Main.java
@@ -3,24 +3,37 @@ package ft.etachott;
 import ft.etachott.builder.GameControllerBuilder;
 import ft.etachott.controller.GameController;
 import ft.etachott.view.ConsoleView;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-import java.util.Objects;
-
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java -jar swingy.jar [console | gui]");
-        }
-        GameControllerBuilder gameControllerBuilder = new GameControllerBuilder();
-        switch (args[0]) {
-            case "gui":
-                System.out.println("gui!");
-            case "console":
-            default:
-                GameController consoleController = gameControllerBuilder
-                        .setGameView(new ConsoleView())
-                        .build();
-                consoleController.run();
-        }
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            if (args.length != 1) {
+                System.err.println("Usage: java -jar swingy.jar [console | gui]");
+                return;
+            }
+            GameControllerBuilder gameControllerBuilder = new GameControllerBuilder();
+            switch (args[0]) {
+                case "gui":
+                    System.out.println("gui!");
+                    break;
+                case "console":
+                default:
+                    GameController consoleController = gameControllerBuilder
+                            .setGameView(new ConsoleView())
+                            .build();
+                    consoleController.run();
+            }
+        };
     }
 }
