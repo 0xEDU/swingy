c9a7374b12a7cf3b60b6b5e0e8702b4c44b2852e
etachott <bfwaend@gmail.com>
2024-06-07 21:42:29 -0300

    Update Character model

--- src/main/java/ft/etachott/model/Character.java
+++ src/main/java/ft/etachott/model/Character.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Characters")
+@Table(name = "characters")
 public class Character {
     @Id
     @GeneratedValue
